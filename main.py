1ff6d4155750667a9d39e471c1cc7595ef9d080c
JosephJvB <joevanbo@pm.me>
2020-02-11 15:49:17 +1300

    make reaction roles generic in prep for platform roles

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import discord
 from discord import utils
 from bot import Bot
 
-# reaction_name : role_id 
-platform_map = { # no platform roles atm
-    '442691013664833550': '', # switch
-    '442691013664833550': '', # pc
-    '442691013664833550': '', # playstation
-    '442691013664833550': '', # xbox
-}
-region_map = {
-    'na': 331436384349061124, # NA
-    'eu': 331436493643972619, # eu
-    'oce': 441311109501419541, # oce
-    'Asia': 334295825368743936 # asia
+# msgid: { reaction_name : role_id }
+msg_map = {
+    # no platform roles atm
+    # int(os.getenv('platformreactmsg')): {
+    #     '_name': 'platform',
+    #     'ntswitch': 99,
+    #     'bnet': 99,
+    #     'ps4': 99,
+    #     'xbox': 99,
+    # },
+    int(os.getenv('regionreactmsg')): {
+        '_name': 'region',
+        'na': 331436384349061124,
+        'eu': 331436493643972619,
+        'oce': 441311109501419541,
+        'Asia': 334295825368743936,
+    }
 }
 
 class MyClient(discord.Client):
@@ -26,7 +31,6 @@ class MyClient(discord.Client):
             '!codes': self._bot.cmd_codes
         }
         self.ready = True
-        self.region_msg = int(os.getenv('regionreactmsg'))
         self.react_channel = int(os.getenv('reactchannel'))
         self.wallride_guild = int(os.getenv('guild'))
 
@@ -48,25 +52,29 @@ class MyClient(discord.Client):
     async def on_raw_reaction_add(self, payload):
         if not self.ready:
             return
-        if payload.message_id != self.region_msg:
+        if payload.message_id in msg_map.keys():
+            await self.handle_reaction_roles(payload)
+        else:
             return
+
+    async def handle_reaction_roles(self, payload):
+        _map = msg_map.get(payload.message_id)
+        _msg_name = _map.get('_name')
 
         g = self.get_guild(self.wallride_guild)
         user = g.get_member(payload.user_id)
-        existing = [r for r in user.roles if r.id in region_map.values()]
+        c = self.get_channel(self.react_channel)
+        m = await c.fetch_message(payload.message_id)
+        existing = [r for r in user.roles if r.id in _map.values()]
 
         if payload.emoji.name == 'trash': # trash all -> exit
-            print(f'trashing roles for {user.display_name}')
-            c = self.get_channel(self.react_channel)
-            m = await c.fetch_message(self.region_msg)
+            print(f'trashing {_msg_name} for {user.display_name}')
             for r in m.reactions: # todo: parallel web requests
                 await r.remove(user)
             await user.remove_roles(*existing)
             return
 
         if len(existing) > 0: # remove existing reactions & roles -> continue
-            c = self.get_channel(self.react_channel)
-            m = await c.fetch_message(self.region_msg)
             r_names = [r.name for r in existing]
             print(f'removing {r_names} from {user.display_name}')
             to_remove = [r for r in m.reactions if r.emoji.name != payload.emoji.name]
@@ -74,12 +82,13 @@ class MyClient(discord.Client):
                 await r.remove(user)
             await user.remove_roles(*existing)
 
-        requested_role = region_map.get(payload.emoji.name)
+        requested_role = _map.get(payload.emoji.name)
         if requested_role: # add requested role
             to_add = g.get_role(requested_role)
             await user.add_roles(to_add)
             print(f'added [{to_add.name}] to {user.display_name}')
-
+        else:
+            print(f'No role found for emojiname={payload.emoji.name}, msg={_msg_name}')
 
 client = MyClient()
 client.ready = False
